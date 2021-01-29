bb8091dc077a22d94c88466204bab06e3a2281c3
Liam O'Boyle <liam@elyobo.net>
2021-01-30 08:14:03 +1100

    Use webpack externals to avoid bundling node dependencies.
    This resolves the problem with file paths changing (and avoids redundant
    bundling anyway).

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   mode: 'production',
   target: 'node',
   entry: './index.js',
+  externals: [nodeExternals()],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build'),
